9f8b1eb6149eb592a206553f7a6e9436398baa26
damiangohrh123 <damiangohrh@gmail.com>
2023-07-05 14:25:52 +0800

    Refactor code to use classes

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ UPDATE:
 4. Did not focus on CSS styling. Will continue to follow up next time.
 5. Overall quite an easy project. Learnt more about prototypes and constructors. 
 6. However, need to brush up on array functions. Seem to forget most of them. (E.g. splt, push)
+
+UPDATE:
+1. Refactored code to use classes. 
+2. Code seems much neater now.
--- index.js
+++ index.js
@@ -1,107 +1,108 @@
-const btnAddBook = document.querySelector('.btnAddBook');
-const addBookForm = document.querySelector('#addBookForm');
-const library = document.querySelector('.library');
+class DOMElements {
+  constructor() {
+    this.btnAddBook = document.querySelector('.btnAddBook');
+    this.addBookForm = document.querySelector('#addBookForm');
+    this.library = document.querySelector('.library');
+  }
+}
 
-//Library array
-let myLibrary = [];
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+  }
+
+  toggleRead() {
+    this.read = !this.read;
+    library.render();
+  }
+}
+
+class Library {
+  myLibrary = [];
+
+  //Add a new book to myLibrary and display it on DOM Tree
+  addBookToLibrary() {
+    let title = document.querySelector('#title').value;
+    let author = document.querySelector('#author').value;
+    let pages = document.querySelector('#pages').value;
+    let read = document.querySelector('#read').checked;
+
+    let newBook = new Book(title, author, pages, read);
+    this.myLibrary.push(newBook);
+
+    this.render();
+  }
+
+  render() {
+    domElements.library.textContent = '';
+
+    for (let i = 0; i < this.myLibrary.length; i++) {
+      const book = this.myLibrary[i];
+  
+      const bookElement = document.createElement('div');
+      const titleElement = document.createElement('div');
+      const authorElement = document.createElement('div');
+      const pagesElement = document.createElement('div');
+      const readElement = document.createElement('div');
+
+      titleElement.textContent = `Title: ${book.title}`;
+      authorElement.textContent = `Author: ${book.author}`;
+      pagesElement.textContent = `Pages: ${book.pages}`;
+      readElement.textContent = book.read;
+  
+      bookElement.appendChild(titleElement);
+      bookElement.appendChild(authorElement);
+      bookElement.appendChild(pagesElement);
+      bookElement.appendChild(readElement);
+  
+      //Remove button
+      const btnRemove = document.createElement("button");
+      btnRemove.textContent = 'Remove';
+      btnRemove.classList.add = 'btnRemove';
+      btnRemove.addEventListener('click', () => {
+        const index = i;
+        this.removeBook(index);
+      });
+      bookElement.appendChild(btnRemove);
+  
+      //Toggle Read button
+      const btnToggleRead = document.createElement('button');
+      btnToggleRead.textContent = 'Read';
+      btnToggleRead.classList.add = 'read';
+      btnToggleRead.addEventListener('click', () => {
+        const index = i;
+        this.myLibrary[index].toggleRead();
+      });
+      bookElement.appendChild(btnToggleRead);
+  
+      domElements.library.appendChild(bookElement);
+    }
+  }
+
+    //Remove book from myLibrary array
+  removeBook(i) {
+    this.myLibrary.splice(i, 1);
+    this.render();
+  }
+}
+
+const library = new Library();
+const domElements = new DOMElements();
 
 //Add book button (Displays form when btnAddBook is clicked on)
-btnAddBook.addEventListener('click', () => {
-  addBookForm.style.display = 'block';
+domElements.btnAddBook.addEventListener('click', () => {
+  domElements.addBookForm.style.display = 'block';
 });
 
 //Add a new book to myLibrary when form is submitted
-addBookForm.addEventListener('submit', (e) => {
+domElements.addBookForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  addBookToLibrary();
+  library.addBookToLibrary();
 });
 
-//Book constructor
-function Book (title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
-}
 
-//Read status
-Book.prototype.toggleRead = function() {
-  this.read = !this.read;
-  render();
-}
 
-//Remove book from myLibrary array
-Book.prototype.removeBook = function(i) {
-  myLibrary.splice(i, 1);
-  render();
-}
 
-//Add a new book to myLibrary and display it on DOM Tree
-function addBookToLibrary() {
-  let title = document.querySelector('#title').value;
-  let author = document.querySelector('#author').value;
-  let pages = document.querySelector('#pages').value;
-  let read = document.querySelector('#read').checked;
-
-  let newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
-  
-  render();
-}
-
-//Function for appending book properties to DOM Tree
-function render() {
-  while(library.firstChild) {
-    library.removeChild(library.firstChild);
-  }
-
-  for (let i = 0; i < myLibrary.length; i++) {
-    let book = myLibrary[i];
-
-    //Book Element
-    const bookElement = document.createElement('div');
-
-    //Title
-    const titleElement = document.createElement('div');
-    titleElement.textContent = `Title: ${book.title}`;
-    
-    //Author
-    const authorElement = document.createElement('div');
-    authorElement.textContent = `Author: ${book.author}`;
-
-    //Pages
-    const pagesElement = document.createElement('div');
-    pagesElement.textContent = `Pages: ${book.pages}`;
-
-    //Read
-    const readElement = document.createElement('div');
-    readElement.textContent = book.read;
-
-    bookElement.appendChild(titleElement);
-    bookElement.appendChild(authorElement);
-    bookElement.appendChild(pagesElement);
-    bookElement.appendChild(readElement);
-
-    //Remove button
-    const btnRemove = document.createElement("button");
-    btnRemove.textContent = 'Remove';
-    btnRemove.classList.add = 'btnRemove';
-    btnRemove.addEventListener('click', () => {
-      const index = i;
-      myLibrary[index].removeBook(index);
-    });
-    bookElement.appendChild(btnRemove);
-
-    //Toggle Read button
-    const btnToggleRead = document.createElement('button');
-    btnToggleRead.textContent = 'Read';
-    btnToggleRead.classList.add = 'read';
-    btnToggleRead.addEventListener('click', () => {
-      const index = i;
-      myLibrary[index].toggleRead();
-    });
-    bookElement.appendChild(btnToggleRead);
-
-    library.appendChild(bookElement);
-  }
-}
